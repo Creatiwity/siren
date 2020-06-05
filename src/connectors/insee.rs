88b0aa57a70b95de97183c1441a5c13db046a238
Julien Blatecky <julien.blatecky@creatiwity.net>
2020-06-05 16:13:35 +0200

    Remove camelCase

--- src/connectors/insee.rs
+++ src/connectors/insee.rs
@@ -70,79 +70,79 @@ fn default_as_false() -> bool {
     false
 }
 
-#[allow(non_snake_case)]
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 struct Header {
     total: u32,
     debut: u32,
     nombre: u32,
     curseur: String,
-    curseurSuivant: String,
+    curseur_suivant: String,
 }
 
-#[allow(non_snake_case)]
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 struct PeriodeUniteLegale {
-    dateFin: Option<String>,
-    dateDebut: String,
-    etatAdministratifUniteLegale: String,
-    changementEtatAdministratifUniteLegale: bool,
-    nomUniteLegale: Option<String>,
-    changementNomUniteLegale: bool,
-    nomUsageUniteLegale: Option<String>,
-    changementNomUsageUniteLegale: bool,
-    denominationUniteLegale: Option<String>,
-    changementDenominationUniteLegale: bool,
-    denominationUsuelle1UniteLegale: Option<String>,
-    denominationUsuelle2UniteLegale: Option<String>,
-    denominationUsuelle3UniteLegale: Option<String>,
-    changementDenominationUsuelleUniteLegale: bool,
-    categorieJuridiqueUniteLegale: String,
-    changementCategorieJuridiqueUniteLegale: bool,
-    activitePrincipaleUniteLegale: Option<String>,
-    nomenclatureActivitePrincipaleUniteLegale: Option<String>,
-    changementActivitePrincipaleUniteLegale: bool,
-    nicSiegeUniteLegale: String,
-    changementNicSiegeUniteLegale: bool,
-    economieSocialeSolidaireUniteLegale: Option<String>,
-    changementEconomieSocialeSolidaireUniteLegale: bool,
-    caractereEmployeurUniteLegale: Option<String>,
-    changementCaractereEmployeurUniteLegale: bool,
+    date_fin: Option<String>,
+    date_debut: String,
+    etat_administratif_unite_legale: String,
+    changement_etat_administratif_unite_legale: bool,
+    nom_unite_legale: Option<String>,
+    changement_nom_unite_legale: bool,
+    nom_usage_unite_legale: Option<String>,
+    changement_nom_usage_unite_legale: bool,
+    denomination_unite_legale: Option<String>,
+    changement_denomination_unite_legale: bool,
+    denomination_usuelle1_unite_legale: Option<String>,
+    denomination_usuelle2_unite_legale: Option<String>,
+    denomination_usuelle3_unite_legale: Option<String>,
+    changement_denomination_usuelle_unite_legale: bool,
+    categorie_juridique_unite_legale: String,
+    changement_categorie_juridique_unite_legale: bool,
+    activite_principale_unite_legale: Option<String>,
+    nomenclature_activite_principale_unite_legale: Option<String>,
+    changement_activite_principale_unite_legale: bool,
+    nic_siege_unite_legale: String,
+    changement_nic_siege_unite_legale: bool,
+    economie_sociale_solidaire_unite_legale: Option<String>,
+    changement_economie_sociale_solidaire_unite_legale: bool,
+    caractere_employeur_unite_legale: Option<String>,
+    changement_caractere_employeur_unite_legale: bool,
 }
 
-#[allow(non_snake_case)]
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 struct UniteLegale {
     siren: String,
-    statutDiffusionUniteLegale: String,
+    statut_diffusion_unite_legale: String,
 
     #[serde(default = "default_as_false")]
-    unitePurgeeUniteLegale: bool,
+    unite_purgee_unite_legale: bool,
 
-    dateCreationUniteLegale: String,
-    sigleUniteLegale: Option<String>,
-    sexeUniteLegale: Option<String>,
-    prenom1UniteLegale: Option<String>,
-    prenom2UniteLegale: Option<String>,
-    prenom3UniteLegale: Option<String>,
-    prenom4UniteLegale: Option<String>,
-    prenomUsuelUniteLegale: Option<String>,
-    pseudonymeUniteLegale: Option<String>,
-    identifiantAssociationUniteLegale: Option<String>,
-    trancheEffectifsUniteLegale: String,
-    anneeEffectifsUniteLegale: Option<String>,
-    dateDernierTraitementUniteLegale: String,
-    nombrePeriodesUniteLegale: u32,
-    categorieEntreprise: String,
-    anneeCategorieEntreprise: String,
-    periodesUniteLegale: Vec<PeriodeUniteLegale>,
+    date_creation_unite_legale: String,
+    sigle_unite_legale: Option<String>,
+    sexe_unite_legale: Option<String>,
+    prenom1_unite_legale: Option<String>,
+    prenom2_unite_legale: Option<String>,
+    prenom3_unite_legale: Option<String>,
+    prenom4_unite_legale: Option<String>,
+    prenom_usuel_unite_legale: Option<String>,
+    pseudonyme_unite_legale: Option<String>,
+    identifiant_association_unite_legale: Option<String>,
+    tranche_effectifs_unite_legale: String,
+    annee_effectifs_unite_legale: Option<String>,
+    date_dernier_traitement_unite_legale: String,
+    nombre_periodes_unite_legale: u32,
+    categorie_entreprise: String,
+    annee_categorie_entreprise: String,
+    periodes_unite_legale: Vec<PeriodeUniteLegale>,
 }
 
-#[allow(non_snake_case)]
 #[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 struct InseeUniteLegaleResponse {
     header: Header,
-    unitesLegales: Vec<UniteLegale>,
+    unites_legales: Vec<UniteLegale>,
 }
 
 impl Connector {
