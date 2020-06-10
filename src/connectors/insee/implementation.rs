e5ee613fd9118894f30d1c3bab9acc511129f576
Ludovic Douziech <ludovic.douziech@gmail.com>
2020-06-10 14:51:46 +0200

    [SyncInsee:UniteLegale] get legal units from the last treatment date from the db
    - with maximum of 60 days from current date

--- src/connectors/insee/implementation.rs
+++ src/connectors/insee/implementation.rs
@@ -2,29 +2,40 @@ use super::error::InseeUpdateError;
 use super::types::InseeUniteLegaleResponse;
 use super::Connector;
 use crate::models::unite_legale::common::UniteLegale;
-use chrono::{DateTime, Utc};
+use chrono::{Duration, NaiveDate, NaiveDateTime, Utc};
 use reqwest::header::{HeaderValue, ACCEPT, AUTHORIZATION};
 
 const BASE_URL: &str = "https://api.insee.fr/entreprises/sirene/V3";
 
 impl Connector {
+    fn get_minimum_timestamp_for_request(&self, timestamp: NaiveDateTime) -> NaiveDate {
+        let minimum_date: NaiveDate = (Utc::now().naive_local() - Duration::days(60)).date();
+
+        minimum_date.max(timestamp.date())
+    }
+
     pub fn get_daily_unites_legales(
         &self,
-        _start_timestamp: DateTime<Utc>,
+        start_timestamp: NaiveDateTime,
     ) -> Result<Vec<UniteLegale>, InseeUpdateError> {
         let mut unites_legales: Vec<UniteLegale> = vec![];
 
         let client = reqwest::blocking::Client::new();
+
         let mut has_data = true;
         let mut current_cursor = String::from("*");
+        let base_request_url = format!(
+            "{}/siren?q=dateDernierTraitementUniteLegale:[{} TO *]&nombre=1000",
+            BASE_URL,
+            self.get_minimum_timestamp_for_request(start_timestamp)
+        );
+
+        println!("Base Request URL: {}", base_request_url);
 
         while has_data {
             println!("Current cursor: {}", current_cursor);
 
-            let url = format!(
-                "{}/siren?q=dateDernierTraitementUniteLegale:[2020-06-09 TO *]&nombre=1000&curseur={}",
-                BASE_URL, current_cursor
-            );
+            let url = format!("{}&curseur={}", base_request_url, current_cursor);
 
             let response: InseeUniteLegaleResponse = client
                 .get(&url)
