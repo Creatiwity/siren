6f37210ab38062681e37b9a6b017a5a35f64b8ba
Julien Blatecky <julien.blatecky@creatiwity.net>
2021-04-12 17:55:59 +0200

    Fix build with no cache

--- build-musl.sh
+++ build-musl.sh
@@ -2,9 +2,8 @@
 
 set -e
 rustc --version && cargo --version
-chown -R $(id -u):$(id -g) ./target /root/.cargo/registry /root/.cargo/git 2>/dev/null
+chown -R $(id -u):$(id -g) ./target /root/.cargo/registry /root/.cargo/git
 cargo build --release --target x86_64-unknown-linux-musl
-chown -R $(id -u):$(id -g) ./target /root/.cargo/registry /root/.cargo/git 2>/dev/null
 mkdir -p ./dist
 cp target/x86_64-unknown-linux-musl/release/sirene ./dist/
 strip ./dist/sirene
