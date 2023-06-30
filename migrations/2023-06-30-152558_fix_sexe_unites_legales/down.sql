7b1cb9d67f8819becceb3b779db9a884768f2772
Julien Blatecky <julien.blatecky@creatiwity.net>
2023-06-30 18:33:26 +0200

    Fix sexe column in unites_legales

---
+++ migrations/2023-06-30-152558_fix_sexe_unites_legales/down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "public"."unite_legale_staging" ALTER COLUMN "sexe" SET DATA TYPE varchar(1);
+ALTER TABLE "public"."unite_legale" ALTER COLUMN "sexe" SET DATA TYPE varchar(1);
---
+++ migrations/2023-06-30-152558_fix_sexe_unites_legales/up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "public"."unite_legale_staging" ALTER COLUMN "sexe" SET DATA TYPE varchar(4);
+ALTER TABLE "public"."unite_legale" ALTER COLUMN "sexe" SET DATA TYPE varchar(4);
