3524fa98bba42d8b5f6c45b77ed3412d8cd0cb05
Julien Blatecky <julien.blatecky@creatiwity.net>
2025-02-16 18:45:50 +0100

    update CI

--- build-musl.sh
+++ build-musl.sh
@@ -2,7 +2,6 @@
 
 set -e
 rustc --version && cargo --version
-chown -R $(id -u):$(id -g) ./target /root/.cargo/registry /root/.cargo/git
 cargo build --release --target x86_64-unknown-linux-musl
 mkdir -p ./dist
 cp target/x86_64-unknown-linux-musl/release/sirene ./dist/
