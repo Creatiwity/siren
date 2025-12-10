5a98af69a93d80ee0f605b29113debf37f344043
Julien Blatecky <julien.blatecky@creatiwity.net>
2025-12-10 17:32:31 +0100

    Update pipeline

--- build-musl.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -e
-rustc --version && cargo --version
-cargo build --release --target x86_64-unknown-linux-musl
-mkdir -p ./dist
-cp target/x86_64-unknown-linux-musl/release/sirene ./dist/
-strip ./dist/sirene
