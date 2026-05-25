1f3b2276cd70b8fbc63bb53c99a7df5cbec0118d
Julien Blatecky <julien.blatecky@creatiwity.net>
2026-05-25 22:10:43 +0200

    feat: add compression

--- src/commands/serve/runner/mod.rs
+++ src/commands/serve/runner/mod.rs
@@ -56,6 +56,7 @@ pub async fn run(addr: SocketAddr, context: Context) {
                 .allow_origin(tower_http::cors::Any),
         )
         .merge(Scalar::with_url("/scalar", api))
+        .layer(tower_http::compression::CompressionLayer::new())
         .layer(tower_http::trace::TraceLayer::new_for_http())
         .layer(SentryHttpLayer::new().enable_transaction())
         .layer(middleware::from_fn(trace::siren_context_middleware))
