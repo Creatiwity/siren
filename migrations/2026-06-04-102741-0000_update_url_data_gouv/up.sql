d22b3ebfcd6954ca42f410282dc406c9a2ff7c24
Julien Blatecky <julien.blatecky@creatiwity.net>
2026-06-04 12:36:33 +0200

    fix: update data gouv link to use stable ones #646

---
+++ migrations/2026-06-04-102741-0000_update_url_data_gouv/up.sql
@@ -0,0 +1,7 @@
+UPDATE "public"."group_metadata" SET "url" = 'https://www.data.gouv.fr/api/1/datasets/r/0651fb76-bcf3-4f6a-a38d-bc04fa708576' WHERE "group_type" = 'etablissements';
+
+UPDATE "public"."group_metadata" SET "url" = 'https://www.data.gouv.fr/api/1/datasets/r/825f4199-cadd-486c-ac46-a65a8ea1a047' WHERE "group_type" = 'unites_legales';
+
+UPDATE "public"."group_metadata" SET "url" = 'https://www.data.gouv.fr/api/1/datasets/r/9c4d5d9c-4bbb-4b9c-837a-6155cb589e26' WHERE "group_type" = 'liens_succession';
+
+UPDATE "public"."group_metadata" SET "url" = 'https://www.data.gouv.fr/api/1/datasets/r/d8c31348-8630-4918-b6d1-5dd689b05f29' WHERE "group_type" = 'siren_doublons';
